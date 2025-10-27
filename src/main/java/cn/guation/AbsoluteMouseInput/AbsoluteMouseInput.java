8b65ea0e3055404c5e0d7066c5ef3f210249b877
Guation <guation@163.com>
2025-10-27 11:04:03 +0800

    first commit

---
+++ src/main/java/cn/guation/AbsoluteMouseInput/AbsoluteMouseInput.java
@@ -0,0 +1,16 @@
+package cn.guation.AbsoluteMouseInput;
+
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "absolute_mouse_input", useMetadata=true)
+public class AbsoluteMouseInput {
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        System.out.println("Dirt: " + Blocks.dirt.getUnlocalizedName());
+		// Below is a demonstration of an access-transformed class access.
+        System.out.println("Color State: " + new GlStateManager.Color());
+    }
+}
